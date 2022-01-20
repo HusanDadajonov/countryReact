a88f04a64d003ec3c502d6aea8aa946bf6d01bb9
am1rcek <xusandadajonov4385@gmail.com>
2022-01-20 20:41:44 +0500

    wdwdaw

--- src/Nav.js
+++ src/Nav.js
@@ -17,19 +17,8 @@ export default function ({getInpValue}){
             <div className="input-box row align-center">
                 <i className="bx bx-search"></i>
                 <form onSubmit={Search}>
-                    <label htmlFor="country"><input className="search-input" type="text" id="country" placeholder="Search for a country…" onClick={(e)=> SetIitem(e.target)} onChange={(e)=> SetValue(e.target.value)}  /></label>
+                    <label htmlFor="country"><input className="search-input" type="text" id="country" placeholder="Search for a country…" onClick={(e)=> SetIitem(e.target)} onChange={(e)=> SetValue(e.target.value)} autoComplete="off" /></label>
                 </form>
-            </div>
-            <div className="select-btn align-center between">
-                <i className="bx bx-chevron-down"></i>
-                <select name="select-coutries">
-                    <option  disabled defaultValue hidden>Filter by Region</option>
-                    <option>Africa</option>
-                    <option>America</option>
-                    <option>Asia</option>
-                    <option>Europe </option>
-                    <option>Oceania</option>
-                </select>
             </div>
         </div>
     )
